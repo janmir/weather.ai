42d14a217a79d75b5c10582fe7552684d4ca9d97
Jan Miranda <paulzu100@gmail.com>
2017-10-26 16:20:00 +0900

    Hot-swap YAML Values

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 'use strict';
 const request = require("request");
+const moment = require('moment-timezone');
 const $ = require('fast-html-parser');
 const n = require('numjs');
 
@@ -21,13 +22,79 @@ const m = {
         _fn.log("//From Local");
         m.source = _fn.localYamlJson(_var.YAML_FILE);
       }
+
+      //swap templates
+      let keys = _fn.keys(m.source);
+      //Loop all listed sources
+      keys.forEach((key)=>{
+        m.leaf(m.source, key);
+      });
+
+      console.log(m.source);
     }else{
       _fn.log("//From Cache");
     }
-  },
-  pullWebsite: ()=>{
 
+    _fn.log(m.source);
   },
+  pullWebsite: (obj)=>{
+    obj.forEach((element)=>{
+      
+    });
+  },
+  leaf: (base, key, depth='')=>{
+    let obj = base[key];
+
+    if(obj instanceof Array){
+      console.log(depth + "Array");
+
+      var index = 0;
+      obj.forEach((element)=>{
+        m.leaf(obj, index++, depth+'-');
+      });
+    }else if(obj instanceof Object){
+      console.log(depth + "Object");
+      
+      _fn.keys(obj).forEach((element)=>{
+        m.leaf(obj, element, depth+'-');
+      });
+    }else if(typeof obj === "string"){
+      console.log(depth + "String");
+      let str = base[key];
+      
+      //check if match
+      var mtch = str.match(/{([^{}]*)}/g);
+      if(mtch !== null){
+        mtch.forEach((element)=>{
+          let base_element = element;
+          element = element.replace(/[{}]+/g, "");
+
+          var value = "????";
+          let splt = element.split("#");
+          if(splt.length > 1){ //Function call
+            console.log(depth  + '-' + "fn:" + splt[0] + ", args:" + splt[1]);
+
+            let fn = splt[0];
+            let arg = splt[1];
+
+            switch(fn){
+              case 'date':{
+                value = moment().tz('Asia/Tokyo').format(arg);
+              }break;
+            }
+          }else{ //Sibling value
+            console.log(depth  + '-' + "sibling:" + splt[0]);
+
+            //Assign value
+            value = base[splt[0]] || value;
+          }
+
+          //assign new value
+          base[key] = base[key].replace(base_element,value);
+        });
+      }
+    }
+  }
 }
 
 //Main
@@ -42,6 +109,7 @@ module.exports.main = (events, context, callback) => {
   //Set Callback function
   _fn.init(callback)
   
+  var sync = true;
   try{
     //inits
     let action = events.action || "DAILY";
@@ -67,7 +135,11 @@ module.exports.main = (events, context, callback) => {
               //Handle locally
             }break;
             case "website":{
+              //set sync to false
+              sync = false;
+
               //Handle locally
+              m.pullWebsite(obj);
             }break;
             case "video":{
               //Pass to other lambda via sns
@@ -94,6 +166,8 @@ module.exports.main = (events, context, callback) => {
     _fn.handleErrors(error);
   }
 
-  //temp
-  _fn.sexyback(null,{})
+  if(sync){
+    //For sync tasks
+    _fn.sexyback(null,{})
+  }
 };
